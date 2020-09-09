4c733f5c35ad538adaf17d2fa8ad18647b9cc9c1
HCRJeewantha <ravindujxyz@gmail.com>
2020-09-09 17:20:44 +0530

    IT18151220 created hotel detail component

---
+++ src/app/IT18165876/tourguide/tourguide.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TourguideComponent } from './tourguide.component';
+
+describe('TourguideComponent', () => {
+  let component: TourguideComponent;
+  let fixture: ComponentFixture<TourguideComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TourguideComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TourguideComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/IT18165876/tourguide/tourguide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tourguide',
+  templateUrl: './tourguide.component.html',
+  styleUrls: ['./tourguide.component.css']
+})
+export class TourguideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
